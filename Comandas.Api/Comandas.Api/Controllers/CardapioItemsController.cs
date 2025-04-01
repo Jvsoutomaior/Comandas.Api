33ee46a1631aaab920109929f36f1270fbae31f9
Jvsoutomaior <soutomaior950@gmail.com>
2025-04-01 16:56:28 -0300

    Documentação da API com Swagger

--- Comandas.Api/Comandas.Api/Controllers/CardapioItemsController.cs
+++ Comandas.Api/Comandas.Api/Controllers/CardapioItemsController.cs
@@ -2,11 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 using Comandas.Api.Data;
 using Comandas.Api.Models;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Comandas.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Tags("01. CardapioItems")]
     public class CardapioItemsControllers : ControllerBase 
     {
         private readonly ComandasDbContext _context;
@@ -17,12 +19,17 @@ namespace Comandas.Api.Controllers
         }
 
         [HttpGet]
+        [SwaggerOperation(Summary = "retorna uma lista de cardapios", Description = "retorna um IEnumerable de CardapioItem")]
+        [SwaggerResponse(200, "Sucesso", typeof(IEnumerable<CardapioItem>))]
         public async Task<ActionResult<IEnumerable<CardapioItem>>> GetCardapioItems()
         {
             return await _context.CardapioItem.ToListAsync();
         }
 
         [HttpGet("{id}")]
+        [SwaggerOperation(Summary = "retorna um item de cardapio", Description = "retorna um CardapioItem")]
+        [SwaggerResponse(200, "Sucesso", typeof(CardapioItem))]
+        [SwaggerResponse(404, "Não encontrado", null)]
         public async Task<ActionResult<CardapioItem>> GetCardapioItem(int id)
         {
             var cardapioItem = await _context.CardapioItem.FindAsync(id);
@@ -34,6 +41,8 @@ namespace Comandas.Api.Controllers
         }
 
         [HttpPost]
+        [SwaggerOperation(Summary = "Cadastra um novo item de cardapio", Description = "retorna um CardapioItem")]
+        [SwaggerResponse(201, "Sucesso", typeof(CardapioItem))]
         public async Task<ActionResult<CardapioItem>> PostCardapioItem(CardapioItem cardapioItem)
         {
             _context.CardapioItem.Add(cardapioItem);
@@ -42,6 +51,9 @@ namespace Comandas.Api.Controllers
         }
 
         [HttpPut("{id}")]
+        [SwaggerOperation(Summary = "Atualiza um item de cardapio", Description = "retorna um CardapioItem")]
+        [SwaggerResponse(204, "Sucesso", null )]
+        [SwaggerResponse(400, "Requisição inválida", null)]
         public async Task<IActionResult> PutCardapioItem(int id, CardapioItem cardapioItem)
         {
             if (id != cardapioItem.Id)
@@ -54,6 +66,9 @@ namespace Comandas.Api.Controllers
         }
 
         [HttpDelete("{id}")]
+        [SwaggerOperation(Summary = "Remove um item de cardapio", Description = "Realiza exclusão na tabela CardapioItem")]
+        [SwaggerResponse(204, "Sucesso", null)]
+        [SwaggerResponse(404, "Não Encontrado", null)]
         public async Task<IActionResult> DeleteCardapioItem(int id)
         {
             var cardapioItem = await _context.CardapioItem.FindAsync(id);
--- Comandas.Api/Comandas.Api/Program.cs
+++ Comandas.Api/Comandas.Api/Program.cs
@@ -9,7 +9,16 @@ builder.Services.AddDbContext<ComandasDbContext>(options => options.UseNpgsql(co
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "1.0",
+        Title = "Api de Comandas",
+        Description = "Api de comandas em .NET 8"
+    });
+    config.EnableAnnotations();
+});
 
 var app = builder.Build();
 
